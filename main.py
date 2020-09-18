cee2ed10368f9d329a214b78202026613a57891c
YanLu <yfl5541@psu.edu>
2020-09-18 05:57:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Author: Yan Lu yfl5541@psu.edu
+# Collaborator: Spoorthi Dhulappanavar sxd5682@psu.edu
+# Collaborator: Nicole Giron nqg5259@psu.edu
+# Collaborator: Xiaolong Lin xxl5453@psu.edu
+# Section: 4
+# Breakout: 11
+
+def dig_sum(n):
+  if n>10 :
+    return int(n%10 + dig_sum(n/10))
+  else:
+    return n
+
+if __name__ =="__main__":
+  a = int(input("Enter an int: "))
+  print(f"sum of digits of {a} is {dig_sum(a)}.")    
